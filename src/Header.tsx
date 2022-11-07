f5d1b65dc26d89829419581766fcb2b04089b5e5
Shrey Kumar <akashjaat55242@gmail.com>
2022-11-07 07:48:08 +0530

    addedupgrade

--- src/Header.tsx
+++ src/Header.tsx
@@ -4,6 +4,7 @@ function Header()  {
       <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <a href="/"><p className="text-2xl font-medium leading-6 text-gray-900 sm:truncate">X TODO</p></a>
+          <a href="/upgrade"><button className="bn32 rounded-full p-2">Upgrade</button></a>
         </div>
       </div>
     </nav>
